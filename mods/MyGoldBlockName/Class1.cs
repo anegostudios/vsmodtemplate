76ca9b8565cae2b0e0a528005f80fc6784a62140
CreativeMD <CreativeMD@users.noreply.github.com>
2018-03-07 12:28:52 +0100

    Initial commit

---
+++ mods/MyGoldBlockName/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Vintagestory.API.Common;
+
+namespace ClassLibrary1
+{
+    
+    public class MyTestMod : ModBase
+    {
+
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            Console.Write("It works and can be debugged!");
+        }
+    }
+
+}
