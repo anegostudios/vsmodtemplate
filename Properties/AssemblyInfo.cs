23905479ab81bc422ea4d3f75d541f405d5fec2d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2018-05-03 13:27:10 +0200

    Renamed solution and project Removed example mod Updated to VintageStory 1.5.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Vintagestory.API.Common;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServerModWorldEdit")]
+[assembly: AssemblyTitle("VSModLauncher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServerModWorldEdit")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("VSModLauncher")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ModDependency("game")]
+[assembly: ModInfo("RedirectLogs", "redirectlogs", Version = "1.0", Authors = new string[] { "Tyron" },
+        Website = "https://github.com/anegostudios/vsmodtemplate", Description = "Redirecting logs to VS", RequiredOnClient = false)]
