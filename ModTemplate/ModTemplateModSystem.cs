9eeaee4bae6da27611d008d046192334f50d3a91
Th3Dilli <th3dilli@gmx.at>
2025-01-29 19:04:31 +0100

    use Mod.Logger

--- ModTemplate/ModTemplateModSystem.cs
+++ ModTemplate/ModTemplateModSystem.cs
@@ -10,17 +10,17 @@ namespace ModTemplate
         // Called on server and client
         public override void Start(ICoreAPI api)
         {
-            api.Logger.Notification("Hello from template mod: " + Lang.Get("mymodid:hello"));
+            Mod.Logger.Notification("Hello from template mod: " + Lang.Get("mymodid:hello"));
         }
 
         public override void StartServerSide(ICoreServerAPI api)
         {
-            api.Logger.Notification("Hello from template mod server side");
+            Mod.Logger.Notification("Hello from template mod server side");
         }
 
         public override void StartClientSide(ICoreClientAPI api)
         {
-            api.Logger.Notification("Hello from template mod client side");
+            Mod.Logger.Notification("Hello from template mod client side");
         }
     }
 }
